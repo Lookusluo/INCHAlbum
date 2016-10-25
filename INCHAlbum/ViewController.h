7257fd4aab3e6e03c9678f62c40cdfa00158c2aa
lookusluo <comeluo@hotmail.com>
2016-10-25 16:17:19 -0500

    Initial Commit

---
+++ INCHAlbum/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  INCHAlbum
+//
+//  Created by Yazhong Luo on 10/25/16.
+//  Copyright © 2016 Yazhong Luo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
